f6cef282ccc59e60e77252a64ef949c6336f65bd
ASD-theorder <abele4336@gmail.com>
2025-02-07 23:48:00 +0100

    Fixed line endings in requirements.txt

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 from fastapi import FastAPI
 from datetime import datetime
 import pytz # type: ignore
+from starlette.middleware.cors
+import CORSMiddleware # type: ignore
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
     email = "abele4336@gmail.com"
     github_url = "https://github.com/ASD-theorder/Hng12api.git"
-    utc_now = datetime.now(pytz.UTC).isoformat()
+    utc_now = datetime.now(pytz.UTC).strftime("%Y-%m-%dT%H:%M:%SZ")
 
     return {
 "email": email,   # type: ignore
